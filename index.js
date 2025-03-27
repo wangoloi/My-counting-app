b3b6bf1fe0704dd8f25792ac62cadb46cc9f7653
Wangolo Bachawa <wangolobachawa9@gmail.com>
2025-03-27 11:12:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log('hello');
+// console.log('i like coding in javascript')
+
+// window.alert('This is an error') //this is how we print alert messages on the web pages
+
+// window.alert('Go back Please');
+
+document.getElementById("myH1").textContent = "MediHive App"; //this is how we change the text of the element with id my
+document.getElementById("myP1").textContent = "This a management application for drugshops and pharmacies.";
